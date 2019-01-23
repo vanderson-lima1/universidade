830b08fcb12215e2d5b6badca07b6f97150f2e4f
unknown <d.developerfs@gmail.com>
2019-01-22 23:37:08 -0200

    7nd - Correção de bug (load página)

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,4 +1,11 @@
-@extends('layouts.layoutAdminHead')
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/styleCustom.css"> 
+</head>
 
     <body>
 
