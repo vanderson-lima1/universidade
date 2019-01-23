6a4b1bf27260532c439cb9ab64c8a8b4fb5a37da
unknown <d.developerfs@gmail.com>
2019-01-23 10:49:07 -0200

    8nd - Alteração layout (Fontes e Ícones)

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/styleCustom.css"> 
 </head>
@@ -11,10 +14,13 @@
 
             <div class="container-fluid container-fluid-custom">   
                 <nav class="navbar navbar-custom navbar-dark fixed-top bg-dark flex-md-nowrap shadow">
-                    <a class="navbar-brand navbar-brand-custom col-sm-3 col-md-2 mr-0" href="#">Nome da empresa</a>
+                    <a class="navbar-brand navbar-brand-custom col-sm-3 col-md-2 mr-0" href="http://localhost:8000/admin/institutions">Nome da empresa</a>
                     <ul class="navbar-nav px-3">
                         <li class="nav-item text-nowrap">
-                        <a class="nav-link nav-link-custom" href="#">Sair</a>
+                        <a class="nav-link nav-link-custom" href="#">
+                            <i class="material-icons centralizado sm">directions_run</i>
+                            Sair
+                        </a>
                         </li>
                     </ul>
                 </nav>
@@ -24,9 +30,21 @@
             <div id="wrapper">
                 <div id="sidebar-wrapper">
                     <ul class="sidebar-nav">
-                        <li> <a href="http://localhost:8000/admin/institutions">Instituições</a></li>
-                        <li> <a href="http://localhost:8000/admin/unities">Unidades</a></li>
-                        <li> <a href="http://localhost:8000/institution/patients">Pacientes</a></li>    
+                        <li> <a href="http://localhost:8000/admin/institutions">
+                                <i class="material-icons centralizado">location_city</i>
+                                Instituições
+                            </a>
+                        </li>
+                        <li> <a href="http://localhost:8000/admin/unities">
+                                <i class="material-icons centralizado">place</i>
+                                Unidades
+                            </a>
+                        </li>
+                        <li> <a href="http://localhost:8000/institution/patients">
+                                <i class="material-icons centralizado">airline_seat_recline_extra</i>
+                                Pacientes
+                            </a>
+                        </li>    
                     </ul>
                 </div>
 
@@ -34,7 +52,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-lg-12">
-                                <a href="#menu-toggle" class="badge badge-info" id="menu-toggle">Menu</a>
+                                <a href="#menu-toggle" id="menu-toggle"><i class="material-icons menu">swap_horiz</i></a>
                                 <hr>
 
                                 <!-- Conteúdo -->
