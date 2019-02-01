e7ff5375346e86d7bf255facde368efe34fa1da2
Vanderson Lima <vanderson.lima1@gmail.com>
2019-02-01 01:53:20 -0200

    16nd - Inicio erro git

--- app/Util/SessionInformation.php
+++ app/Util/SessionInformation.php
@@ -4,8 +4,13 @@ namespace App\Util;
 use App\Models\Unity;
 use App\Models\Institution;
 
-class SessionInformation 
-{   
+class SessionInformation {
+    
+    CONST ROLE  = [
+        1 => 'Employee',
+        2 => 'Student'
+    ];
+
     // com usuario logado verifica qual é a sua unidade e retorna, acho que essa funcão deve ficar em usario retornando um objeto de unidade
     // ou carregada na mémoria, com final atributos toda vez q o usuário abre a sessão
 
@@ -27,4 +32,10 @@ class SessionInformation
         return $institution;
     }
 
+<<<<<<< HEAD
+=======
+    static function roleLoggedIn() {
+        return ROLE[1].value;
+    }
+>>>>>>> 16nd - Inicio erro git
 }
